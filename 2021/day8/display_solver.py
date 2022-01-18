b21b3d00a874ff1bfcd3724b23b0eb87cbfc7e26
Rebecca Davey <rdav5b@gmail.com>
2022-01-18 22:18:55 +0000

    day 8 solution

--- 2021/day8/display_solver.py
+++ 2021/day8/display_solver.py
@@ -1,3 +1,7 @@
+from collections import Counter
+
+from utils import utils
+
 # """
 #   0:      1:      2:      3:      4:
 #  aaaa    ....    aaaa    aaaa    ....
@@ -24,17 +28,17 @@
 TRUE_SEGMENT_SET = set("ABCDEFG")
 SIGNAL_SEGMENT_SET = set("abcdefg")
 
-DIGIT_SEGMENT_MAP = {
-    0: set("ABCEFG"), # 6 edges
-    1: set("CF"), # 2 edges
-    2: set("ACDEG"), # 5 edges
-    3: set("ACDFG"), # 5 edges
-    4: set("BCDF"), # 4 edges
-    5: set("ABDFG"), # 5 edges
-    6: set("ABDEFG"), # 6 edges
-    7: set("ACF"), # 3 edges
-    8: set("ABCDEFG"), # 7 edges
-    9: set("ABCDFG") # 6 edges
+TRUE_SEGMENT_TO_DIGIT_MAP = {
+    "ABCEFG": 0, # 6 edges
+    "CF": 1, # 2 edges
+    "ACDEG": 2, # 5 edges
+    "ACDFG": 3, # 5 edges
+    "BCDF": 4, # 4 edges
+    "ABDFG": 5, # 5 edges
+    "ABDEFG": 6, # 6 edges
+    "ACF": 7, # 3 edges
+    "ABCDEFG": 8, # 7 edges
+    "ABCDFG": 9 # 6 edges
 }
 
 class DisplaySolver:
@@ -42,39 +46,110 @@ class DisplaySolver:
 
     Args:
         input_signals (list(str)): the ten scrambled input signals
+        debug (bool): whether to print debug statements
     
     Attributes:
         segment_map dict (str -> str): mapping between signal segments and true segments
         solved (bool): whether the mapping has been completely constructed yet
     """
-    def __init__(self, input_signals):
+    def __init__(self, input_signals, debug=False):
         if len(input_signals) != 10:
             raise ValueError("DisplaySolver needs 10 input_signals to initialise")
         
         self.input_signals = input_signals
+        self.debug = debug
+
         self.segment_map = {}
-        self.solved = self._is_solved()
+        self.solve_map()
 
-def _is_solved(self):
-    """Check whether the map has been completely constructed yet
-    
-    Every signal segment exists as a key and every true segment exists
-    as a value in the segment map. Only checks validity, not correctness.
+    def _is_solved(self):
+        """Check whether the map has been completely constructed yet
+        
+        Every signal segment exists as a key and every true segment exists
+        as a value in the segment map. Only checks validity, not correctness.
 
-    Returns:
-        boolean - True if segment map has been built completely, False otherwise
-    """
-    all_keys = all([ss in self.segment_map for ss in SIGNAL_SEGMENT_SET])
-    all_values = all([ts in self.segment_map.values() for ts in TRUE_SEGMENT_SET])
-    return all_keys and all_values
+        Returns:
+            boolean - True if segment map has been built completely, False otherwise
+        """
+        all_keys = all([ss in self.segment_map for ss in SIGNAL_SEGMENT_SET])
+        all_values = all([ts in self.segment_map.values() for ts in TRUE_SEGMENT_SET])
+        
+        if self.debug:
+            print(self.segment_map)
+            print("All keys present? ", all_keys)
+            print("All values present? ", all_values)
 
-def solve_map(self, left_signals):
-    """Apply logic to generate segment map"""
-    # figure out 
+        return all_keys and all_values
 
-def translate_signals(self, signals_to_decode):
-    if not self.solved:
-        raise Exception("Right hand signals cannot be decoded before map is solved")
-    
-    # TODO: put decoding here
-    pass
+    def _get_signals_by_length(self, length):
+        """Return the subset of the input signals with the given length"""
+        return [x for x in self.input_signals if len(x) == length]
+
+    def _get_signals_by_frequency(self, frequency):
+        flat_list = utils.flatten_nested_list(self.input_signals)
+        frequencies = Counter(flat_list)
+        return [char for (char, freq) in frequencies.items() if freq == frequency]
+
+    def _add_mapping(self, signal_segment, true_segment):
+        """This is just here to add debug value without repeating 7 times"""
+        if self.debug:
+            print(f"Adding {signal_segment} -> {true_segment} to segment mapping")
+        self.segment_map[signal_segment] = true_segment
+
+    def solve_map(self):
+        """Apply logic to generate segment map"""
+        if self.debug:
+            print(self.input_signals)
+
+        # A is the signal in the 3-length but not 2-length signals
+        l3 = self._get_signals_by_length(3)[0] # should just be 1
+        l2 = self._get_signals_by_length(2)[0] # should just be 1
+        a = set(l3).difference(set(l2)).pop()
+        self._add_mapping(a, "A")
+
+        # F is the only segment that is in 9 of the 10 digits
+        f = self._get_signals_by_frequency(9)[0]
+        self._add_mapping(f, "F")
+
+        # B is the only segment in exactly 6 of the 10 digits
+        b = self._get_signals_by_frequency(6)[0]
+        self._add_mapping(b, "B")
+
+        # E is the only segment in exactly 4 of the 10 digits
+        e = self._get_signals_by_frequency(4)[0]
+        self._add_mapping(e, "E")
+
+        # now we know F, C is the one in the length-2 signal that isn't F
+        c = set(l2).difference({f}).pop()
+        self._add_mapping(c, "C")
+
+        # we can find D because it is the only unknown in the length-4 signal
+        l4 = self._get_signals_by_length(4)[0]
+        d = [char for char in l4 if char not in self.segment_map][0]
+        self._add_mapping(d, "D")
+
+        # this means G is the one left
+        g = SIGNAL_SEGMENT_SET.difference(set(self.segment_map.keys())).pop()
+        self._add_mapping(g, "G")
+
+    def translate_signals(self, signals_to_decode):
+        """Use the solved mapping to decode output signals
+
+        Args:
+            signals_to_decode (list(str)): 4 output signals
+
+        Returns:
+            list(int): the corresponding 4 digits, decoded
+        """
+        if not self._is_solved():
+            raise Exception(f"""
+                Right hand signals cannot be decoded before
+                map is solved. Current map: {self.segment_map}
+            """)
+        
+        output_digits = []
+        for output_signal in signals_to_decode:
+            true_segments = "".join(sorted([self.segment_map[char] for char in output_signal]))
+            output_digits.append(TRUE_SEGMENT_TO_DIGIT_MAP[true_segments])
+
+        return output_digits
--- 2021/day8/script.py
+++ 2021/day8/script.py
@@ -1,13 +1,26 @@
-from utils.utils import load_input
+from utils import utils
 
 from display_solver import DisplaySolver
 
 
 def part1(data):
-    return -1
+    # input should be list of tuples where tuple[0] is a list of the input
+    # signals (10 of them) and tuple[1] is a list of the output signals (4)
+    digits_to_count = [1, 4, 7, 8]
+    counter = 0
+    for input_signals, output_signals in data:
+        solver = DisplaySolver(input_signals, debug=False)
+        output_digits = solver.translate_signals(output_signals)
+        counter += len([d for d in output_digits if d in digits_to_count])
+    return counter
 
 def part2(data):
-    return -1
+    output_sum = 0
+    for input_signals, output_signals in data:
+        solver = DisplaySolver(input_signals, debug=False)
+        output_digits = solver.translate_signals(output_signals)
+        output_sum += int("".join([str(d) for d in output_digits]))
+    return output_sum
 
 def parse_line(l):
     """Take a full line and split into intput & output signals"""
@@ -18,14 +31,13 @@ def parse_line(l):
 
 if __name__ == "__main__":
     # load example and real input data
-    example_input = load_input(path="example.txt", parsing_func=parse_line)
-    print(example_input)
-    real_input = load_input()
+    example_input = utils.load_input(path="example.txt", parsing_func=parse_line)
+    real_input = utils.load_input(parsing_func=parse_line)
 
     # part 1
     assert part1(example_input) == 26
     print("Part 1: {}".format(part1(real_input)))
 
     # part2
-    # assert part2(example_input) == ???
-    # print("Part 2: {}".format(part2(real_input)))
+    assert part2(example_input) == 61229
+    print("Part 2: {}".format(part2(real_input)))
